0bd57a6ad49142c39db2594589e3803fbcf390a9
Avery Maxwell <averymaxwell42@gmail.com>
2023-01-24 22:57:47 -0600

    Add playRound() and game() functions

--- script.js
+++ script.js
@@ -3,18 +3,27 @@ function getComputerChoice() {
     let randomArray = ["Rock", "Paper", "Scissors"];
     let numberOfChoices = randomArray.length;
     let randomNumber = Math.floor(Math.random() * numberOfChoices);
-    let randomChoice = randomArray[randomNumber];
-    return randomChoice;
+    let computerSelection = randomArray[randomNumber];
+    return computerSelection;
 }
 
 function playRound(playerSelection, computerSelection) {
-    
-    let computerSelection = getComputerChoice;
-
-    if ((playerSelection === "rock" && computerSelection === "paper") || (playerSelection === "paper" && computerSelection === "scissors") || (playerSelection === "scissors" && computerSelection === "rock")) {
+    if ((playerSelection === "Rock" && computerSelection === "Paper") || (playerSelection === "Paper" && computerSelection === "Scissors") || (playerSelection === "Scissors" && computerSelection === "Rock")) {
         return alert("You Lose! " + computerSelection + " beats " + playerSelection);
+    } else if (playerSelection == computerSelection) {
+        return alert("It\'s a Tie! " + playerSelection + " ties " + computerSelection);
     } else {
-        return alert("You Win!" + playerSelection + " beats " + computerSelection);
+        return alert("You Win! " + playerSelection + " beats " + computerSelection);
+    }
+}
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+
     }
 
+    if (xx)
+    return 
 }
+
+// playRound(prompt("Rock, Paper, or Scissors?"), getComputerChoice());
