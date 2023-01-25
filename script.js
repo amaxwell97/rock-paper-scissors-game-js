99df9e23c7362a07a329461403b5444d5af61496
Avery Maxwell <averymaxwell42@gmail.com>
2023-01-24 20:58:20 -0600

    Update randomNumber and add return line

--- script.js
+++ script.js
@@ -2,5 +2,6 @@
 function getComputerChoice() {
     let randomArray = ["Rock", "Paper", "Scissors"];
     let numberOfChoices = randomArray.length;
-    let randomNumber = Math.floor(Math.random() * numberOfChoices);
+    let randomNumber = Math.ceil(Math.random() * numberOfChoices);
+    return randomChoice = randomArray[randomNumber];
 }
